b0756718a61ace3cace53366b1057cde75582a57
inderjeet <inderjeetofc@gmail.com>
2021-12-02 17:04:26 +0530

    styled cart screen with product in acrt details

--- front-end/src/screens/CartScreen.js
+++ front-end/src/screens/CartScreen.js
@@ -1,27 +1,90 @@
-import React, { useEffect} from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useLocation, useParams } from "react-router";
 import { addToCart } from "../state/actions/cartActions";
+import { Link } from "react-router-dom";
 
 export default function CartScreen(props) {
   const dispatch = useDispatch();
   let productId = useParams().id;
-//   console.log(productId,typeof(productId))
-//   productId= Number(productId)
-//   console.log(productId,typeof(productId))
+  //   console.log(productId,typeof(productId))
+  //   productId= Number(productId)
+  //   console.log(productId,typeof(productId))
   let Qty = useLocation().search;
   Qty = Number(Qty.split("=")[1]);
+  const cart = useSelector((state) => state.cart);
+  const cartItem = cart.cartItem;
+  console.log(cartItem);
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, Qty));
     }
   }, [dispatch, productId, Qty]);
+
+  const removeFromCartHandler = () => {
+    //delete cart pdt
+  };
+
   return (
     <div>
-      <h1>CART SCREEN</h1>
-      <div className="row">
-        <p>Product Id : {productId}</p>
-        <p>qty : {Qty}</p>
+      <div className="row screen-width card_body">
+        <div className="col-2">
+          <h1>Shopping Cart</h1>
+          {cartItem.length === 0 ? (
+            <div>
+              cart is empty<Link to="/">Go Shopping</Link>
+            </div>
+          ) : (
+            <ul>
+              {cartItem.map((pdt) => (
+                <li keys={pdt.cartData.item}>
+                  <div className="row">
+                    <div>
+                      <img
+                        src={pdt.cartData.image}
+                        alt={pdt.cartData.name}
+                        className="small"
+                      ></img>
+                    </div>
+                    <div className="min-30">
+                      <Link
+                        className="color-black"
+                        to={`/product/${productId}`}
+                      >
+                        {pdt.cartData.name}
+                      </Link>
+                    </div>
+                    <div>
+                      <select
+                        value={cartItem.qty}
+                        onChange={(e) =>
+                          dispatch(
+                            addToCart(pdt.cartData.id, Number(e.target.value))
+                          )
+                        }
+                      >
+                        {[...Array(pdt.cartData.inStock).keys()].map((e) => (
+                          <option key={e + 1} value={e + 1}>
+                            {e + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <div>${pdt.cartData.price}</div>
+                    <div>
+                      <button
+                        type="button"
+                        onClick={removeFromCartHandler(pdt.cartData.id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
